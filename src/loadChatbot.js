fee08883489ab211b10103d134b2b08f6cdafd9e
Ho Mak <homak120@gmail.com>
2025-03-16 10:31:56 -0400

    Some content and color adjustment

--- src/loadChatbot.js
+++ src/loadChatbot.js
@@ -18,13 +18,13 @@ function RenderChatbot() {
       </button>
       <div class="chatbot" style="z-index: 9999;">
         <header>
-          <h2>Digital Teller</h2>
+          <h2>Virtual Assistant</h2>
           <span class="close-btn material-symbols-outlined">close</span>
         </header>
         <ul class="chatbox">
           <li class="chat incoming">
             <span class="material-symbols-outlined">smart_toy</span>
-            <p>Hi there 👋<br />I’m your Digital Teller. I’m here to help you open an account and answer any questions along the way. How can I assist you today?</p>
+            <p>Hi there 👋<br />I’m your Virtual Assistant. I’m here to help you open an account and answer any questions along the way. How can I assist you today?</p>
           </li>
         </ul>
         <div class="chat-input">
